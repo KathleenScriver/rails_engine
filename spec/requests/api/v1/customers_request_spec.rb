3618872acf0ce7c6adc25395289caa644dfd4e50
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-01 18:00:42 -0600

    Adds test for customer index

---
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe "Customer API" do
+  it 'sends list of all customers' do
+    create_list(:customers, 4)
+
+    get '/api/v1/customers'
+
+    expect(response).to be_successful
+    expect(response.body.count).to eq(4)
+  end
+end
