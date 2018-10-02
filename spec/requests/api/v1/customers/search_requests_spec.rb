88f6d7f6bbdff92c3b3a0e4fd32e42534c327d36
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-02 11:50:17 -0600

    Adds customer search by last name

--- spec/requests/api/v1/customers/search_requests_spec.rb
+++ spec/requests/api/v1/customers/search_requests_spec.rb
@@ -12,4 +12,15 @@ describe "Customer Search API" do
     expect(response).to be_successful
     expect(customer["first_name"]).to eq(first_name)
   end
+
+  it 'finds single record by last name' do
+    last_name = create(:customer).last_name
+
+    get '/api/v1/customers/find?last_name', params: {last_name: "#{last_name}"}
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["last_name"]).to eq(last_name)
+  end
 end
