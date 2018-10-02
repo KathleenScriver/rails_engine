9c1776e7ada027148329704f3b6bf4c3f6511b63
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-02 15:41:16 -0600

    Add transaction migration, model, and relationships

---
+++ db/migrate/20181002213620_create_transactions.rb
@@ -0,0 +1,10 @@
+class CreateTransactions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :transactions do |t|
+      t.string :result
+      t.references :invoice, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002212332) do
+ActiveRecord::Schema.define(version: 20181002213620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 20181002212332) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.string "result"
+    t.bigint "invoice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
