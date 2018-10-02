e6d234987d848464adddb31f1d916036fe5aa94c
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-02 15:27:01 -0600

    Makes db changes, add relationships for merchants, customers, and invoices

--- db/migrate/20181002212332_create_invoices.rb
+++ db/migrate/20181002212332_create_invoices.rb
@@ -2,6 +2,7 @@ class CreateInvoices < ActiveRecord::Migration[5.1]
   def change
     create_table :invoices do |t|
       t.string :status
+      t.references :merchant, foreign_key: true
       t.references :customer, foreign_key: true
 
       t.timestamps
