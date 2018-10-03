b4941617f8d33e350659a77f1f755ebeb83be255
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-02 18:42:57 -0600

    Adds search for invoice by status

--- app/controllers/api/v1/invoices/search_controller.rb
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::Invoices::SearchController < ApplicationController
 
   private
   def search_params
-    params.permit(:id)
+    params.permit(:id, :status)
   end
 end
--- spec/requests/api/v1/invoices/invoice_search_spec.rb
+++ spec/requests/api/v1/invoices/invoice_search_spec.rb
@@ -12,4 +12,15 @@ describe "Invoice Search API" do
     expect(response).to be_successful
     expect(result["id"]).to eq(id)
   end
+
+  it 'should find single invoice by status' do
+    invoice = create(:invoice)
+
+    get '/api/v1/invoices/find?status', params: {status: invoice.status}
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(result["id"]).to eq(invoice.id)
+  end
 end
