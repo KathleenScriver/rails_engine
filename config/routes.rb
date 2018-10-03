469eef62de62a59778cd99d60505c4a38b70681c
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-03 16:25:25 -0600

    Refactors routes to include namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'customers/find', to: 'customers/search#show'
-      get 'customers/find_all', to: 'customers/search#index'
-      get 'customers/:id/favorite_merchant', to: 'customers/favorite_merchant#show'
-      get 'customers/random', to: 'customers/random#show'
+
+      namespace :customers do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get ':id/favorite_merchant', to: 'favorite_merchant#show'
+        get 'random', to: 'random#show'
+      end
+
+      namespace :invoices do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+      end
+
       resources :customers, only: [:index, :show]
-      get 'invoices/find', to: 'invoices/search#show'
-      get 'invoices/find_all', to: 'invoices/search#index'
       resources :invoices, only: [:index, :show]
     end
   end
