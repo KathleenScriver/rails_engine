cf53bd12b50325ff27dd09c3d5b26a5d3345e9c4
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-04 11:44:17 -0600

    Working on revenue by date - in progress

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -22,4 +22,12 @@ class Merchant < ApplicationRecord
      .order("total_items desc")
      .limit(number)
   end
+
+  def self.revenue_by_date(date)
+    joins(invoices: [:invoice_items, :transactions])
+     .select("sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+     .merge(Transaction.unscoped.success)
+     .where(created_at: (date.to_datetime.beginning_of_day..date.to_datetime.end_of_day)
+     
+  end
 end
