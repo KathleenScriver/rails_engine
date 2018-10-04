bfe74e4aca681d1a9e839043cdf45c459f3d25de
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-04 13:44:07 -0600

    Adds model test for Invoice.all_items

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:invoice_items) }
     it { should have_many(:items), through: :invoice_items }
   end
+
+  describe 'class methods' do
+    context '.all_items' do
+      it 'should return all items associated with single invoice' do
+        invoice = create(:invoice)
+        item_1 = create(:item)
+        item_2 = create(:item)
+        item_3 = create(:item)
+        item_4 = create(:item)
+        item_5 = create(:item)
+        invoice_item_1 = create(:invoice_item, invoice_id: invoice.id, item_id: item_1.id)
+        invoice_item_1 = create(:invoice_item, invoice_id: invoice.id, item_id: item_2.id)
+        invoice_item_1 = create(:invoice_item, invoice_id: invoice.id, item_id: item_4.id)
+        invoice_item_1 = create(:invoice_item, invoice_id: invoice.id, item_id: item_5.id)
+
+        expect(Invoice.all_items(invoice.id)).to eq([item_1, item_2, item_4, item_5])
+      end
+    end
+  end
 end
