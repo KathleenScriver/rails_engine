216ad5b3c2c7dee602a958e3a3b24baf27e95463
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-04 12:37:56 -0600

    Finishes test for invoice transaction endpoint

--- spec/requests/api/v1/invoices/relationship_requests_spec.rb
+++ spec/requests/api/v1/invoices/relationship_requests_spec.rb
@@ -1,10 +1,18 @@
 require "rails_helper"
 
 describe "Invoices API Relationship Endpoints" do
-  before(:each) do
 
-  end
   it 'should return all associated transactions' do
+    invoice = create(:invoice)
+    transactions = create_list(:transaction, 12, invoice_id: invoice.id)
+    transaction_99 = create(:transaction)
 
+    get "/api/v1/invoices/#{invoice.id}/transactions"
+
+    resulting_transactions = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(resulting_transactions.count).to eq(12)
+    expect(resulting_transactions.pluck(:id).include?(transaction_99.id)).to be_falsey
   end
 end
