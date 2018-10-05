19ea395001dd544e5f8d20ef181d26f0ab546e78
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-05 12:18:05 -0600

    Adds customer serializer to display total revenue

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
-    render json: Merchant.revenue_by_date(params[:date])
+    render json: Merchant.revenue_by_date(params[:date]), serializer: MerchantRevenueSerializer
   end
 end
