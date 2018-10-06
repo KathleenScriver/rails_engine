44700282c7ad1187b584208760bd83366e5a1cbe
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-05 21:10:11 -0600

    Adds route and show action to controller, changes all merchants revenue to index

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
-  def show
+  def index
     render json: Merchant.revenue_by_date(params[:date]), serializer: MerchantRevenueSerializer
   end
+
+  def show
+
+  end 
 end
